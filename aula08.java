3d989049efb430ea75faac157c1eb31c3998e1d8
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-06-22 13:27:02 -0300

    Add files via upload

---
+++ aula08.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class aula08 {
+public static void main(String[] args) {
+	int idade = 15;
+	if (idade < 18) {
+		System.out.println("Nao pode entrar");
+	}else {
+		System.out.println("pode entrar");
+	}
+}
+}
