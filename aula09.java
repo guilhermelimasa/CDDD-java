095b9710c2c1d22845f34703ac16da0fe1e5072d
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-06-22 13:27:27 -0300

    Add files via upload

---
+++ aula09.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class aula09 {
+public static void main(String[] args) {
+	int idade = 15;
+	boolean amigodoDno = true;
+	if (idade < 18 && amigodoDno == false) {
+		System.out.println("não pode entrar");
+	}	
+		else {
+	System.out.println("Pode entrar");
+		}
+}
+}
+
