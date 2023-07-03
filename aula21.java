13acdda9df3e0c652fd3a54af256995f21ac1625
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-07-03 15:28:00 -0300

    Add files via upload

---
+++ aula21.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class aula21 {
+public static void main(String[] args) {
+	int cont = 0;
+	int numero = 100;
+	while (cont<numero) {
+		cont = cont + 1;
+		if (cont % 2 != 0) {
+			System.out.println(cont);
+	}
+}
+}
+}
