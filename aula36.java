8b5cc3729f93c8e528de4f84bb11ed01e73c47ad
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-07-12 15:11:42 -0300

    Add files via upload

---
+++ aula36.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class aula36 {
+public static void main(String[] args) {
+	String valor = "CDD - Java";
+		System.out.println(valor.endsWith("Java"));
+		System.out.println(valor.startsWith("C"));
+		System.out.println(valor.startsWith("DD",1));
+		
+}
+}
