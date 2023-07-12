c54b74c0795ed614623b9e8f1090547d0eb5693a
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-07-12 15:14:55 -0300

    Add files via upload

---
+++ aula42.java
@@ -0,0 +1,8 @@
+package fundamentos;
+
+public class aula42 {
+public static void main(String[] args) {
+	String texto = "Será que são iguais?";
+	System.out.println(texto.compareTo("Será que são iguais?")== 0? true : false);
+}
+}
