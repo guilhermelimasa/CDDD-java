81084efe9a15e009561c64ad0e102efd95698de9
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-07-12 15:13:47 -0300

    Add files via upload

---
+++ aula39.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class aula39 {
+public static void main(String[] args) {
+	
+	String texto = " texto para retirar espaços no início e fim";
+	String renome = texto.replace(" texto para retirar espaços no início e fim ","texto para retirar espaços no início e fim");
+	System.out.println(renome);
+}
+}
