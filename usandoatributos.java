da0bdd2b5bcc5d1341c92f120bf7453c9254e86b
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-07-20 15:44:32 -0300

    Add files via upload

---
+++ usandoatributos.java
@@ -0,0 +1,41 @@
+package encapsulamento;
+
+public class usandoatributos{
+	public static void main(String[] args) {
+		numeros n1 = new numeros();
+		numeros n2 = new numeros();
+		numeros n3 = new numeros();
+		numeros n4 = new numeros();
+		numeros n5 = new numeros();
+		numeros n6 = new numeros();
+		numeros n7 = new numeros();
+		numeros n8 = new numeros();
+		numeros n9 = new numeros();
+		numeros n10 = new numeros();
+		n1.setNum1(1);
+		System.out.println(n1.getNum1());
+		n2.setNum2(2);
+		System.out.println(n2.getNum2());
+		n3.setNum3(3);
+		System.out.println(n3.getNum3());
+		n4.setNum4(4);
+		System.out.println(n4.getNum4());
+		n5.setNum5(5);
+		System.out.println(n5.getNum5());
+		n6.setNum6(6);
+		System.out.println(n6.getNum6());
+		n7.setNum7(7);
+		System.out.println(n7.getNum7());
+		n8.setNum8(8);
+		System.out.println(n8.getNum8());
+		n9.setNum9(9);
+		System.out.println(n9.getNum9());
+		n10.setNum10(10);
+		System.out.println(n10.getNum10());
+
+		
+		
+	
+	}
+		
+}
